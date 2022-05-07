a6ae703825420a9e4b4a440860de3b8992e74d9a
TanuWebDevloper <97585825+TanuWebDevloper@users.noreply.github.com>
2022-05-07 12:25:30 +0530

    first commit

---
+++ src/main/java/com/jpa/demo/Spring4Application.java
@@ -0,0 +1,15 @@
+package com.jpa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4Application.class, args);
+		
+		System.out.println("working");
+	} 
+
+}
